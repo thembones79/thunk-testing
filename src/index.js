ccf6bbbb72578e18f78466273f0f6be1dd789f1a
thembones79 <michal.szumnarski@gmail.com>
2020-01-25 17:49:49 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import Reddit from './Reddit';
+import { getPosts } from './actions';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  isLoading: false,
+  error: null,
+  posts: []
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'GET_POSTS_BEGIN':
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      };
+    case 'GET_POSTS_SUCCESS':
+      return {
+        ...state,
+        posts: action.posts,
+        isLoading: false
+      };
+    case 'GET_POSTS_ERROR':
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer, applyMiddleware(thunk));
+store.dispatch(getPosts());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Reddit />
+  </Provider>,
+  document.querySelector('#root')
+);
